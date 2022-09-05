4e2cc2bf7810e69ff830221ec0dd7d3ec9f0b011
cppizarro <cppizarro@miuandes.cl>
2022-09-05 00:16:12 -0300

    textarea

--- project2_app/static/js/base1.js
+++ project2_app/static/js/base1.js
@@ -291,3 +291,62 @@ document
 
     //     stage.add(layer);
     // });
+
+    document
+    .getElementById('btnCreatePoints')
+    .addEventListener('click', function () {
+
+
+          var simpleText = new Konva.Text({
+            x: 25,
+            y: 25,
+            text: 'texto', //que esto sea un input
+            fontSize: 15,
+            fontFamily: 'Calibri',
+            fill: 'green',
+            draggable: true
+          });
+          layer.add(simpleText);
+          
+          simpleText.on('dblclick dbltap', () => {
+            // create textarea over canvas with absolute position
+    
+            // first we need to find position for textarea
+            // how to find it?
+    
+            // at first lets find position of text node relative to the stage:
+            var textPosition = simpleText.getAbsolutePosition();
+    
+            // then lets find position of stage container on the page:
+            var stageBox = stage.container().getBoundingClientRect();
+    
+            // so position of textarea will be the sum of positions above:
+            var areaPosition = {
+              x: stageBox.left + textPosition.x,
+              y: stageBox.top + textPosition.y,
+            };
+    
+            // create textarea and style it
+            var textarea = document.createElement('textarea');
+            document.body.appendChild(textarea);
+    
+            textarea.value = simpleText.text();
+            textarea.style.position = 'absolute';
+            textarea.style.top = areaPosition.y + 'px';
+            textarea.style.left = areaPosition.x + 'px';
+            textarea.style.width = simpleText.width();
+    
+            textarea.focus();
+    
+            textarea.addEventListener('keydown', function (e) {
+              // hide on enter
+              if (e.keyCode === 13) {
+                simpleText.text(textarea.value);
+                document.body.removeChild(textarea);
+              }
+            });
+          });
+          
+
+        stage.add(layer);
+    });
