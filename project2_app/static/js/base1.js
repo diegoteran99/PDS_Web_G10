78111040f5fab0c9ac2517689b55d6032fa035a2
cppizarro <cppizarro@miuandes.cl>
2022-09-04 23:43:04 -0300

    can edit charge values

--- project2_app/static/js/base1.js
+++ project2_app/static/js/base1.js
@@ -110,6 +110,44 @@ document
           });
           layer.add(simpleText);
           
+          simpleText.on('dblclick dbltap', () => {
+            // create textarea over canvas with absolute position
+    
+            // first we need to find position for textarea
+            // how to find it?
+    
+            // at first lets find position of text node relative to the stage:
+            var textPosition = simpleText.getAbsolutePosition();
+    
+            // then lets find position of stage container on the page:
+            var stageBox = stage.container().getBoundingClientRect();
+    
+            // so position of textarea will be the sum of positions above:
+            var areaPosition = {
+              x: stageBox.left + textPosition.x,
+              y: stageBox.top + textPosition.y,
+            };
+    
+            // create textarea and style it
+            var textarea = document.createElement('textarea');
+            document.body.appendChild(textarea);
+    
+            textarea.value = simpleText.text();
+            textarea.style.position = 'absolute';
+            textarea.style.top = areaPosition.y + 'px';
+            textarea.style.left = areaPosition.x + 'px';
+            textarea.style.width = simpleText.width();
+    
+            textarea.focus();
+    
+            textarea.addEventListener('keydown', function (e) {
+              // hide on enter
+              if (e.keyCode === 13) {
+                simpleText.text(textarea.value);
+                document.body.removeChild(textarea);
+              }
+            });
+          });
           const anchor1 = new Konva.Circle({
             x: arrow.points()[0],
             y: arrow.points()[1],
