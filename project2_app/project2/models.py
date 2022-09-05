e4cb10ddc3c307d880f705c06601d264562fd392
cppizarro <cppizarro@miuandes.cl>
2022-09-05 01:02:42 -0300

    base de datos

--- project2_app/project2/models.py
+++ project2_app/project2/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    difficulty = models.IntegerField()
+
+    def __str__(self):
+        return self.description
+
+
+
+class Dcl(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
--- project2_app/project2/views.py
+++ project2_app/project2/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from project2.models import Task
+
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
@@ -8,4 +10,6 @@ def index(request):
 
 def all_tasks(request):
     # llamar a la db y obtener todas las tareas...
-    return render(request, 'all_tasks.html')
+    tasks_list = Task.objects.all()
+    context = {'tasks_list':tasks_list}
+    return render(request, 'all_tasks.html', context)
